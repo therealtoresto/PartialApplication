fc5b184aaa919771ef4889a554b1ee196f6ab527
TORESTO <therealtoresto@gmail.com>
2020-04-28 16:47:50 +0300

    creating

---
+++ 3-bind.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+const log = (base, n) => Math.log(n) / Math.log(base);
+
+//Usage
+
+const lg = log.bind(null, 10);
+const ln = log.bind(null, Math.E);
+
+console.log('lg(5) = ' + lg(5));
+console.log('ln(5) = ' + ln(5));
